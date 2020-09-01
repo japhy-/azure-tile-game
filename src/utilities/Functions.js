af233a864e67aa9c554998e4e1753ad9e119b49a
Jeffrey Pinyan <jeffrey.pinyan@ithreat.com>
2020-08-31 21:18:53 -0400

    moved common functions to utilities

--- src/utilities/Functions.js
+++ src/utilities/Functions.js
@@ -15,3 +15,20 @@ export const forN = (start, end=undefined, step=undefined) => (
 // forN(10,12) === [10,11,12]
 // forN(0,10,2) === [0,2,4,6,8,10] === 2x[0,1,2,3,4,5]
 // forN(10,19,3) === [10,13,16,19] === 10+3x[0,1,2,3]
+
+
+export const shuffle = (array) => {
+  array = [...array];
+  for (let i = array.length-1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    if (j !== i) [ array[j], array[i] ] = [ array[i], array[j] ];
+  }
+  return array;
+};
+
+
+export const divide = (array, filter) => {
+  const partitions = [ [], [] ];
+  array.forEach(elem => partitions[filter(elem) ? 0 : 1].push(elem));
+  return partitions;
+};
